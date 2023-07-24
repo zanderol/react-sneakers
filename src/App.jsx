7449d7bf5fbc9b64fe9e96c60579820957328558
ZanderOl <olexandruk@gmail.com>
2023-07-24 13:24:53 +0300

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          {" "}
+          <img width={40} height={40} src="/img/header_logo.png" />
+          <div>
+            <h3 className="text-uppercase">React sneakers</h3>
+            <p className="opacity-5">Best sport shoes store</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} height={18} src="/img/cart.svg" />
+            <span> 120 USD</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/img/user.svg" />
+          </li>
+        </ul>
+      </header>
+      <div className="content p-40">
+        <h1 className="mb-40">All shoes</h1>
+        .....
+        <div className="d-flex">
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/1.jpg"
+              alt="Sneakers"
+            />
+            <h5>Men&apos;s shoes Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Price:</span>
+                <b>129.99 USD</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Add" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/2.jpg"
+              alt="Sneakers"
+            />
+            <h5>Men&apos;s shoes Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Price:</span>
+                <b>129.99 USD</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Add" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/3.jpg"
+              alt="Sneakers"
+            />
+            <h5>Men&apos;s shoes Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Price:</span>
+                <b>129.99 USD</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Add" />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/4.jpg"
+              alt="Sneakers"
+            />
+            <h5>Men&apos;s shoes Nike Blazer Mid Suede</h5>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column ">
+                <span>Price:</span>
+                <b>129.99 USD</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/img/plus.svg" alt="Add" />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
