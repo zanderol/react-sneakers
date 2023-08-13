55ca6c315a459517bf4379be67e975518a6be13b
ZanderOl <olexandruk@gmail.com>
2023-08-13 23:17:11 +0300

    add render of elements

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,69 @@ import Card from "./components/Card";
 import Drawer from "./components/Drawer";
 import Header from "./components/Header";
 
+const goods = [
+  {
+    title: "Men's shoes Nike Blazer Mid Suede",
+    price: 129.99,
+    imageUrl: "/img/sneakers/1.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 129.99,
+    imageUrl: "/img/sneakers/2.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 84.99,
+    imageUrl: "/img/sneakers/3.jpg",
+  },
+  {
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 89.99,
+    imageUrl: "/img/sneakers/4.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Under Armour Curry 8",
+    price: 151.99,
+    imageUrl: "/img/sneakers/5.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Kyrie 7",
+    price: 112.99,
+    imageUrl: "/img/sneakers/6.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Jordan Air Jordan 11",
+    price: 107.99,
+    imageUrl: "/img/sneakers/7.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike LeBron XVIII",
+    price: 164.99,
+    imageUrl: "/img/sneakers/8.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Lebron XVIII Low",
+    price: 139.99,
+    imageUrl: "/img/sneakers/9.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 84.99,
+    imageUrl: "/img/sneakers/10.jpg",
+  },
+  {
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 89.99,
+    imageUrl: "/img/sneakers/11.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Kyrie Flytrap IV",
+    price: 112.99,
+    imageUrl: "/img/sneakers/12.jpg",
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -18,61 +81,9 @@ function App() {
         </div>
         .....
         <div className="d-flex">
-          <Card />
-          {/* <div className="card">
-            <img
-              width={133}
-              height={112}
-              src="/img/sneakers/2.jpg"
-              alt="Sneakers"
-            />
-            <h5>Men&apos;s shoes Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column ">
-                <span>Price:</span>
-                <b>129.99 USD</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Add" />
-              </button>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              width={133}
-              height={112}
-              src="/img/sneakers/3.jpg"
-              alt="Sneakers"
-            />
-            <h5>Men&apos;s shoes Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column ">
-                <span>Price:</span>
-                <b>129.99 USD</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Add" />
-              </button>
-            </div>
-          </div>
-          <div className="card">
-            <img
-              width={133}
-              height={112}
-              src="/img/sneakers/4.jpg"
-              alt="Sneakers"
-            />
-            <h5>Men&apos;s shoes Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column ">
-                <span>Price:</span>
-                <b>129.99 USD</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Add" />
-              </button>
-            </div>
-          </div> */}
+          {goods.map((obj) => (
+            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
-function Card() {
+function Card(props) {
   return (
     <div className="card">
       <div className="favorite">
         <img src="/img/heart-neutral.svg" alt="Neutral" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-      <h5>Men&apos;s shoes Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column ">
           <span>Price:</span>
-          <b>129.99 USD</b>
+          <b>{props.price}</b>
         </div>
         <button className="button">
           <img width={11} height={11} src="/img/plus.svg" alt="Add" />
